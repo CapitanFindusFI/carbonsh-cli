f16916bc530790f633c04d0bd63ecf13cc2ac8ef
Karim ElAzzouni <k.elazzouni@live>
2020-10-07 22:22:06 +0200

    Introduce dir parameter

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { fstat } from "fs";
 import path from "path";
 import yargs from "yargs";
 import fs from "fs";
 import CarbonCLIController from './cli/controllers/CarbonCLIController';
 import { DefaultTheme, ThemesList } from './types/themes.enum';
+import FileUtils from "./utils/FileUtils";
 
 const args = yargs.option('f', {
     alias: 'file',
@@ -13,7 +13,16 @@ const args = yargs.option('f', {
     describe: 'The carbon.now.sh theme to use, defaults to "seti"',
     default: DefaultTheme,
     choices: ThemesList
+}).option('d', {
+    alias: 'dir',
+    describe: 'Path to a directory that has code files'
 }).argv;
+
+// should not have both file and dir arguments
+if (args.file && args.dir) {
+    console.error("Cannot have both file and dir arguments passed.");
+    process.exit(1);
+}
 
 const SCREENSHOTS_FOLDER = path.join('screenshots');
 try {
@@ -24,7 +33,14 @@ try {
 
 let controller = new CarbonCLIController();
 try {
-    controller.getScreenshot(args);
+    if (args.dir) {
+        var files:string[] = FileUtils.traverseDirectoryAndReturnListOfFiles(args.dir);
+        files.forEach((f) => {
+            controller.getScreenshot({f});
+        });
+    } else {
+        controller.getScreenshot(args);
+    }
 } catch (e) {
     console.error(e)
     process.exit(-1)
